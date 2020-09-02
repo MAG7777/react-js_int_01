5e30b83613232c18e0b633615f5cb19335072bf4
Armen <armen.movsisyan@chessify.me>
2020-09-02 23:44:26 +0400

    Remove log

--- src/Product.js
+++ src/Product.js
@@ -6,7 +6,6 @@ import Description from "./utils/Description";
 export default class Product extends React.Component {
 
     render() {
-        console.log("AAA===>>", this.props)
         const info = this.props.info;
         return (
             <div style={{textAlign:"left"}}>
