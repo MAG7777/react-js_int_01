943cba04b1abee268e3d2ae8e839c0eedf77ca1b
Armen <armen.movsisyan@chessify.me>
2020-09-10 17:10:12 +0400

    Minor fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import Product from "./Product";
 
 const productInfo = {
@@ -11,12 +10,9 @@ const productInfo = {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Product info={productInfo} />
-      </header>
-    </div>
+    <>
+      <Product info={productInfo} />
+    </>
   );
 }
 
